6b28ad3b7df88f955bd27754dc185b2671c161b6
Guilherme Nunes <deoliveiranunes.guilherme@hotmail.com>
2023-08-22 15:48:37 -0300

    Ajustanto script

--- script.sh
+++ script.sh
@@ -17,14 +17,22 @@ sudo apt-get install unzip -y
 sudo apt-get install zip -y
 
 
-echo "Clonando aplicacao no repositorio /tmp..."
+echo "Apagando index default do Apache..."
+
+cd /var/www/html
+sudo rm index.html
+
+
+echo "Baixando aplicacao no repositorio /tmp..."
 
 cd /tmp
-git clone https://github.com/denilsonbonatti/linux-site-dio.git
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+sudo unzip main.zip
 
 
 echo "Copiando a aplicacao para a pasta html do Apache..."
 
+cd linux-site-dio-main/
 sudo cp -R * /var/www/html/
 
 
